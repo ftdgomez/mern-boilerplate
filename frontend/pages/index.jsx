743bd38f3c36f3d1b8273dd11993ca4199d957a4
Daniel Gomez <dgcodedev@gmail.com>
2020-12-30 04:00:45 +0100

    feat(pages): add some style to index page

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,15 +1,19 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <div> 
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    <main>
-      <p>Hello World</p>
-    </main>
-   </div>
-  )
+	return (
+		<div className='bg-primary'>
+			<Head>
+				<title>Mern Bolerplate Nextjs</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<main className='container'>
+				<div className='min-h-screen flex items-center justify-center'>
+					<p className='text-white font-bold'>
+						Mern Bolerplate - Next js v-1.0.0
+					</p>
+				</div>
+			</main>
+		</div>
+	);
 }
