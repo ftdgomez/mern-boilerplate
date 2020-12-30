2ab1e4952941435971712b0c8eaa2a77914601c3
Daniel Gomez <dgcodedev@gmail.com>
2020-12-30 03:51:03 +0100

    feat(pages): add politicas de privacidad page and some style components

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,7 @@
-import 'tailwindcss/tailwind.css'
 import { UserContext, UserProvider } from '../context/userContext'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import '../global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
